2d7e8942d4d2549a70270ba9e90fc0d7f96e9900
Fenhl <fenhl@fenhl.net>
2024-03-30 22:47:07 +0000

    Remove unnecessart bounds from EnumSet impl

--- crate/async-proto/src/impls/enumset.rs
+++ crate/async-proto/src/impls/enumset.rs
@@ -9,7 +9,7 @@ use {
 };
 
 #[derive(Protocol)]
-#[async_proto(internal)]
+#[async_proto(internal, where())]
 struct EnumSetProxy<T: EnumSetType + EnumSetTypeWithRepr>(<T as EnumSetTypeWithRepr>::Repr)
 where <T as EnumSetTypeWithRepr>::Repr: Protocol + Send + Sync;
 
@@ -30,7 +30,7 @@ where <T as EnumSetTypeWithRepr>::Repr: Protocol + Send + Sync {
 impl_protocol_for! {
     #[async_proto(attr(cfg_attr(docsrs, doc(cfg(feature = "enumset")))))]
     #[async_proto(attr(doc = "The type will be read via [`from_repr_truncated`](enumset::EnumSet::from_repr_truncated), ignoring invalid variants."))]
-    #[async_proto(via = EnumSetProxy<T>)]
+    #[async_proto(via = EnumSetProxy<T>, where())]
     type EnumSet<T: EnumSetType + EnumSetTypeWithRepr>
     where <T as EnumSetTypeWithRepr>::Repr: Protocol + Send + Sync;
 }
