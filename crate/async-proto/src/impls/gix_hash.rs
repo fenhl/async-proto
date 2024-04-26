e5b2d6716bccf1760f6e82948632717558679541
Fenhl <fenhl@fenhl.net>
2024-04-26 01:43:09 +0000

    Skip enum discriminator for gix-hash::ObjectId

--- crate/async-proto/src/impls/gix_hash.rs
+++ crate/async-proto/src/impls/gix_hash.rs
@@ -1,8 +1,29 @@
-use async_proto_derive::impl_protocol_for;
+use {
+    gix_hash::ObjectId,
+    async_proto_derive::impl_protocol_for,
+    crate::Protocol,
+};
+
+#[derive(Protocol)]
+#[async_proto(internal)]
+struct OidProxy([u8; 20]);
+
+impl From<OidProxy> for ObjectId {
+    fn from(OidProxy(sha1): OidProxy) -> Self {
+        Self::Sha1(sha1)
+    }
+}
+
+impl<'a> From<&'a ObjectId> for OidProxy {
+    fn from(oid: &ObjectId) -> Self {
+        match *oid {
+            ObjectId::Sha1(sha1) => Self(sha1),
+        }
+    }
+}
 
 impl_protocol_for! {
     #[async_proto(attr(cfg_attr(docsrs, doc(cfg(feature = "gix-hash")))))]
-    enum gix_hash::ObjectId {
-        Sha1([u8; 20]),
-    }
+    #[async_proto(via = OidProxy)]
+    type ObjectId;
 }
