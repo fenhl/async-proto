144e1bbddc1ab22d8f912bd374834885451bb1d5
Fenhl <fenhl@fenhl.net>
2024-04-26 01:21:19 +0000

    Checkin pre-commit check

---
+++ assets/pre-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+cargo check
+cargo check --manifest-path=crate/async-proto/Cargo.toml --features=tokio-tungstenite
+cargo check --manifest-path=crate/async-proto/Cargo.toml --features=tungstenite
+cargo check --manifest-path=crate/async-proto/Cargo.toml --features=warp
+cargo check --manifest-path=crate/async-proto/Cargo.toml --features=bytes,chrono,chrono-tz,either,enumset,git2,noisy_float,semver,serde_json,uuid
+cargo test --manifest-path=crate/async-proto/Cargo.toml --all-features
+cargo check --target=i686-pc-windows-msvc
+cargo test --manifest-path=crate/async-proto/Cargo.toml --target=i686-pc-windows-msvc --all-features
+cargo doc --all-features
