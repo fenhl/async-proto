ea9a7da989f5a29d4fec8a45fe9d4f71cf30e45f
Fenhl <fenhl@fenhl.net>
2025-08-13 18:03:20 +0000

    Simplify chrono_tz::Tz impl

--- crate/async-proto/src/impls/chrono_tz.rs
+++ crate/async-proto/src/impls/chrono_tz.rs
@@ -1,34 +1,11 @@
 use {
     async_proto_derive::impl_protocol_for,
-    crate::{
-        Protocol,
-        ReadErrorKind,
-    },
+    crate::ReadErrorKind,
 };
-
-#[derive(Protocol)]
-#[async_proto(internal)]
-struct TzProxy {
-    name: String,
-}
-
-impl TryFrom<TzProxy> for chrono_tz::Tz {
-    type Error = ReadErrorKind;
-
-    fn try_from(TzProxy { name }: TzProxy) -> Result<Self, ReadErrorKind> {
-        name.parse::<Self>().map_err(|e| ReadErrorKind::Custom(e.to_string()))
-    }
-}
-
-impl<'a> From<&'a chrono_tz::Tz> for TzProxy {
-    fn from(tz: &chrono_tz::Tz) -> Self {
-        Self { name: tz.name().to_owned() }
-    }
-}
 
 impl_protocol_for! {
     #[async_proto(attr(cfg_attr(docsrs, doc(cfg(feature = "chrono-tz")))))]
     #[async_proto(attr(doc = "A timezone is represented as an [IANA timezone identifier](https://data.iana.org/time-zones/theory.html#naming)."))]
-    #[async_proto(via = TzProxy)]
+    #[async_proto(as_string, map_err = |e: chrono_tz::ParseError| ReadErrorKind::Custom(e.to_string()))]
     type chrono_tz::Tz;
 }
