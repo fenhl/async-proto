1a63270bc57bec630ae9ecc73f7854fd9cbe5154
Fenhl <fenhl@fenhl.net>
2026-01-30 01:53:22 +0000

    Impl for os_info::Type

--- crate/async-proto/src/impls/os_info.rs
+++ crate/async-proto/src/impls/os_info.rs
@@ -1,6 +1,38 @@
-use async_proto_derive::impl_protocol_for;
+use {
+    async_proto_derive::impl_protocol_for,
+    crate::{
+        Protocol,
+        ReadErrorKind,
+        WriteErrorKind,
+    },
+};
+
+#[derive(Protocol)]
+#[async_proto(internal)]
+struct TypeProxy(String);
+
+impl TryFrom<TypeProxy> for os_info::Type {
+    type Error = ReadErrorKind;
+
+    fn try_from(TypeProxy(s): TypeProxy) -> Result<Self, Self::Error> {
+        serde_plain::from_str(&s).map_err(|e| ReadErrorKind::Custom(e.to_string()))
+    }
+}
+
+impl<'a> TryFrom<&'a os_info::Type> for TypeProxy {
+    type Error = WriteErrorKind;
+
+    fn try_from(ty: &os_info::Type) -> Result<Self, Self::Error> {
+        Ok(Self(serde_plain::to_string(ty).map_err(|e| WriteErrorKind::Custom(e.to_string()))?))
+    }
+}
 
 impl_protocol_for! {
+    #[async_proto(attr(cfg_attr(docsrs, doc(cfg(feature = "os_info")))))]
+    #[async_proto(attr(doc = "An OS type is represented as its enum variant name."))]
+    #[async_proto(via = TypeProxy)]
+    type os_info::Type;
+
     #[async_proto(attr(cfg_attr(docsrs, doc(cfg(feature = "os_info")))))]
     enum os_info::Version {
         Unknown,
